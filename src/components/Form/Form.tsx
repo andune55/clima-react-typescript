020734d8d922d8261ec063b460dfcc98d17e3802
aab <latierramedia@gmail.com>
2025-03-15 16:42:35 +0100

    curso 265

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useState } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 import type { SearchType } from "../../Types"
 import { countries } from "../../data/countries"
 import styles from "./Form.module.css"
+import Alert from "../Alert/Alert"
 
 export default function Form() {
 
@@ -9,6 +10,7 @@ export default function Form() {
         city: '',
         country: ''
     })
+    const [alert, setAlert] = useState('')
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) =>{
         setSearch({
@@ -16,8 +18,20 @@ export default function Form() {
             [e.target.name] : e.target.value //escribimos en el campo en el cual estamos modificando
         })
     }
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(Object.values(search).includes('')){
+            //console.log('Sí hay campos vacíos...')
+            setAlert('Todos los campos son obligatorios')
+            return
+        }
+    }
     return (
-        <form className={styles.form}>
+        <form 
+            className={styles.form}
+            onSubmit={handleSubmit}
+        >
+            {alert && <Alert>{alert}</Alert>}
 
             <div className={styles.field}>
                 <label htmlFor="city">Ciudad:</label>
