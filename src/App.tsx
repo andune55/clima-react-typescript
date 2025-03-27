b6737c0948540d4e82b53fec824cdb5bc53b9421
aab <latierramedia@gmail.com>
2025-03-27 17:44:03 +0100

    Fin proyecto clima 280

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Form from './components/Form/Form'
 import useWeather from "./hooks/useWeather"
 import WeatherDetail from './components/WeatherDetail/WeatherDetail';
 import Spinner from "./components/Spinner/Spinner";
+import Alert from "./components/Alert/Alert";
 
 function App() {
   
-  const { fetchWeather, weather, loading, hasWeatherData } = useWeather()
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } = useWeather()
 
   //console.log(import.meta.env)
 
@@ -21,11 +22,8 @@ function App() {
         />
         
         {loading && <Spinner />}
-        {hasWeatherData &&
-          <WeatherDetail
-            weather = {weather}
-          />
-        }
+        {hasWeatherData && <WeatherDetail weather = {weather}/>}
+        {notFound && <Alert>Ciudad No encontrada</Alert>}
         
       </div>
     </>
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -50,17 +50,23 @@ const initialState = {
 export default function useWeather(){
 
     const [weather, setWeather] = useState<Weather>(initialState)
-
     const [loading, setLoading] = useState(false)
+    const [notFound, setNotFound] = useState(false)
 
     const fetchWeather = async (search: SearchType) => {
         const appId = import.meta.env.VITE_API_KEY
         setLoading(true)
         setWeather(initialState)
         try {
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`  
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`  
             console.log(geoUrl)
             const {data} = await axios.get(geoUrl)
+
+            //Comprobar si existe
+            if(!data[0]){
+                setNotFound(true)
+                return
+            }
             //console.log(data)
             const lat= data[0].lat
             const lon= data[0].lon
@@ -118,6 +124,7 @@ export default function useWeather(){
     return {
         weather,
         loading,
+        notFound,
         fetchWeather,
         hasWeatherData
     }
